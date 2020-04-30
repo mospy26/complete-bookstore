4c9822cd91b59394b265dd5142aeb11cd5aa41d3
mospy26 <m.mustafaahmed@hotmail.com>
2020-04-30 20:50:51 +1000

    Revert "Logic for deducting stocks from warehouse completed (PLZ VERIFY MUSTAFA)"
    This reverts commit 652f80fec707d1aa9d6da60cb5ded4c031938940.

--- BookStore.Entities/BookStore.Business.Entities/Partial/Order.cs
+++ BookStore.Entities/BookStore.Business.Entities/Partial/Order.cs
@@ -9,45 +9,40 @@ namespace BookStore.Business.Entities
     {
         public void UpdateStockLevels()
         {
+
             // TODO Warehouse stuff
+
             foreach (OrderItem lItem in this.OrderItems)
             {
-                int? amountOfItemToDeduct = lItem.Quantity;
+                int lTotalQuantity = lItem.Quantity;
+                int? lTotalStock = lItem.Book.Stocks.Sum(r => r.Quantity);
 
-                foreach (Stock stock in lItem.Book.Stocks.OrderByDescending(r => r.Quantity))
-                {
-                    if (amountOfItemToDeduct == 0)
-                    {
-                        break;
-                    } else if (amountOfItemToDeduct < stock.Quantity) 
-                    {
-                        amountOfItemToDeduct = 0;
-                        stock.Quantity -= amountOfItemToDeduct;
-                        break;
-                    } else
-                    {
-                        amountOfItemToDeduct -= stock.Quantity;
-                        stock.Quantity = 0;
-                    }
-                }
-
-                if (amountOfItemToDeduct != 0)
+                // Not enough stock
+                if (lTotalStock.Value < lTotalQuantity)
                 {
                     throw new Exception("Cannot place an order - This book is out of stock");
                 }
-            }
 
-            //foreach (OrderItem lItem in this.OrderItems)
-            //{
-            //    if (lItem.Book.Stock.Quantity - lItem.Quantity >= 0)
-            //    {
-            //        lItem.Book.Stock.Quantity -= lItem.Quantity;
-            //    }
-            //    else
-            //    {
-            //        throw new Exception("Cannot place an order - This book is out of stock");
-            //    }
-            //}
+                // There is stock
+                foreach (Stock lStock in lItem.Book.Stocks.OrderByDescending(r => r.Quantity))
+                {
+                    if (lTotalQuantity == 0)
+                    {
+                        break;
+                    }
+                    if (lStock.Quantity - lTotalQuantity >= 0)
+                    {
+                        lStock.Quantity -= lTotalQuantity;
+                        lTotalQuantity = 0;
+                    }
+                    else if (lStock.Quantity < lTotalQuantity)
+                    {
+                        int? temp = lStock.Quantity;
+                        lStock.Quantity = 0;
+                        lTotalQuantity -= temp.Value;
+                    }
+                }
+            }
         }
     }
 }
