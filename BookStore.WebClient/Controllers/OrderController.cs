b8330442c2830621b3c4b712d8c46b0326b74601
Brendon Lam <brendonlam123@hotmail.com>
2020-05-04 22:58:59 +1000

    cshtml file and controller done (need to implement to service)

--- BookStore.WebClient/Controllers/OrderController.cs
+++ BookStore.WebClient/Controllers/OrderController.cs
@@ -5,14 +5,24 @@ using System.Web;
 using System.Web.Mvc;
 using BookStore.WebClient.ViewModels;
 
-namespace BookStore.WebClient.Controllers
+namespace BookStore.WebClient.Controllers45
 {
     public class OrderController : Controller
     {
         // GET: Order
+
         public ActionResult Index(UserCache pUserCache)
         {
-            return View(new ListOrderItemViewModel(pUserCache.Model));
+            ListOrderItemViewModel ListOrderItemViewModel = new ListOrderItemViewModel(pUserCache.Model);
+            return View(ListOrderItemViewModel);
+        }
+
+        public ActionResult DeleteOrder(int pOrderId, String pReturnUrl, UserCache pUserCache)
+        {
+            // Do Something 
+            ServiceFactory.Instance.OrderService.CancelOrder(pOrderId);
+
+            return RedirectToAction("Index", new { pUserCache = pUserCache });
         }
     }
 }
